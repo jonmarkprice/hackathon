4dae9d9654445519e8b8807eef988a0c52d34b1a
Hieu Le <trunghieule94@gmail.com>
2016-03-19 14:31:54 -0500

    Add scripts for main

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,21 @@
-var main = function (argument) {
-	var buy_button = document.getElementById()
+window.onload {
+	var buyButton = document.getElementById('buy_button');
+	buyButton.onclick = buyFunction;
+
+	var sellButton = document.getElementById('sell_button');
+	sellButton.onclick = sellFunction;
+}
+
+function buyFunction() {
+	var buyInfo = document.getElementById('buy_info');	
+	buyInfo.style.display = '';
+	var sellInfo = document.getElementById('sell_info');
+	sellInfo.style.display = 'none';
+}
+
+function sellFunction() {
+	var buyInfo = document.getElementById('buy_info');	
+	buyInfo.style.display = 'none';
+	var sellInfo = document.getElementById('sell_info');
+	sellInfo.style.display = '';
 }
