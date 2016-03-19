48742cfbed13a4671261e7b8fc11646d64eb391a
Jonathan Price <jon.mark.price@gmail.com>
2016-03-19 14:19:30 -0500

    sql "compilers" with sqlite

--- lib/db.sql
+++ lib/db.sql
@@ -1,48 +1,52 @@
-create table account
-{
+/* create schema trubooks; */
+
+drop table if exists account;
+drop table if exists course;
+drop table if exists book;
+drop table if exists for_sale;
+drop table if exists picture;
+
+create table account (
 	name varchar(200),
 	id varchar(200),
 	email varchar(200),
 	phone char(10),
-	#hash char(),
-	#salt char(),
+	/* TODO
+	hash char(),
+	salt char(), */
 	primary key (id)
-};
+);
 
-create table class
-{
+create table course (
 	dept varchar(4),
-	course_num varchar(4),
-	#title varchar(200),
-	primary key (dept, course_num)
-};
+	num varchar(4),
+	/* title varchar(200), */
+	primary key (dept, num)
+);
 
-create table book
-{
+create table book (
 	id integer primary key,
-	class_dept varchar(4),
-	class_course_num varchar(4),
+	course_dept varchar(4),
+	course_num varchar(4),
 	title varchar(200),
 	edition integer,
-	foreign key (class_dept, class_course_num) 
-		references class(dept, course_num)
-};
+	foreign key (course_dept, course_num) 
+		references course(dept, num)
+);
 
-create table for_sale
-{
+create table for_sale (
+	id integer primary key,
 	book_id varchar(200),
 	seller varchar(200),
 	price decimal,
 	picture_file_name varchar(200),
-	id integer primary key,
 	foreign key (book_id) references book(id),
-	foreign key(seller) references account(id)
-};
+	foreign key (seller) references account(id)
+);
 
-create table picture
-{
+create table picture (
 	picture varchar(200),
-	for_sale_id integer
-	foreign key (for_sale_id) references 
-}
+	for_sale_id integer,
+	foreign key (for_sale_id) references for_sale(id)
+);
 
