c15c7826e4504428357f0586000a2bbf185e90f7
Jonathan Price <jon.mark.price@gmail.com>
2016-03-20 11:43:00 -0500

    ajax updates

--- scripts/ajax.js
+++ scripts/ajax.js
@@ -1,21 +1,28 @@
 /*** THIS IS NOT FINISHED OR WORKING CODE ***/
 
 function addResults() {
-	var obj, i;
-	obj = JSON.parse(req.response),
+	var ul, obj, i;
+	obj = JSON.parse(req.response); //!
+	table = createElement("");
 	for (i = 0; i < obj.length; i++) {
-		addBook(obj[i]);
+		addEntry(table, obj[i]);
 	}
+	getElementById
 }
 
-function addBook(data) {
-	//var 
-	createElement("ul");
+function addEntry(table, data) {
+	var row, row_entries;
+	row_entries = ["subj", "", "name"];
+	row = createElement("tr");
+	createElement
 }
 
+
+// AJAX
+var req;
 function sendAJAXRequest() {
-	var req = new XMLHttpRequest();
-	req.open('POST', '', true); // GET or POST?
+	req = new XMLHttpRequest();
+	req.open('POST', 'file.php', true); // GET or POST?
 	req.setRequestHeader("Content-Type", "application/json",) // mb.
 	req.onload = addResults;
 	req.send();
