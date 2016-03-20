994032c370cdcf9d363f0c2281e17ab1d9fc9f8c
Jonathan Price <jon.mark.price@gmail.com>
2016-03-20 14:17:49 -0500

    ed

--- lib/insertExamples.sql
+++ lib/insertExamples.sql
@@ -1,6 +1,4 @@
-insert into book values ('CS', '310', 'Algorithms', '3');
-insert into for_sale values (1, 'jeremy', 30);
-
 insert into account values ('Jeremy', 'Falloon', 'jeremy', 'jf@truman.edu', '1234');
-
+insert into book (course_dept, course_num, title, edition) values ('CS', '310', 'Algorithms', '3');
+insert into for_sale (book_id, seller, price) values (1, 'jeremy', 10.00);
 insert into course values ('CS', '310');
