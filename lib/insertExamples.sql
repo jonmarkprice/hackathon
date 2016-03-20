99890228ad42685605ed57598442119f397fff16
Valerie Free <vaf5451@truman.edu>
2016-03-20 13:51:09 -0500

    created insert statements to test queries

---
+++ lib/insertExamples.sql
@@ -0,0 +1,6 @@
+insert into book values ('CS', '310', 'Algorithms', '3');
+insert into for_sale values (1, 'jeremy', 30);
+
+insert into account values ('Jeremy', 'Falloon', 'jeremy', 'jf@truman.edu', '1234');
+
+insert into course values ('CS', '310');
