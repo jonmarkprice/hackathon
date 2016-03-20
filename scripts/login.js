4bf4d51a6eeb1096be4c834613a62f08e6ed9e90
Hieu Le <trunghieule94@gmail.com>
2016-03-20 13:45:17 -0500

    Added credential verification

--- scripts/login.js
+++ scripts/login.js
@@ -4,11 +4,26 @@ window.onload = function() {
 };
 
 function loginFunction() {
-	var login = document.getElementById('login_button');
 	var username = document.getElementById('username');
 	var usernameValue = username.value;
 	var password = document.getElementById('password');
 	var passwordValue = password.value;
 
+	var loginButton = document.getElementById('login_button');
+	loginButton.onclick = function () {
+		var request = new XMLHttpRequest();
+		var credentials = {
+			username: username,
+			password: password
+		};
+		var data = JSON.stringify(credentials);
+		request.open("POST", "../lib/verify_credential.php", true);
+		request.setRequestHeader('Content-Type', 'application/json');
+		request.send(data);
 
+		var response = JSON.parse(request.response);
+		if (response === "successful") {
+			window.location.href = 'main.html';
+		}
+	};
 }
