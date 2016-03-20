64960f686236c8f57f6a5bd946b1e25ab05f4c5b
Hieu Le <trunghieule94@gmail.com>
2016-03-19 20:52:41 -0500

    Updated main.js and main.html

--- scripts/main.js
+++ scripts/main.js
@@ -7,6 +7,9 @@ window.onload = function() {
 
 	var findBookButton = document.getElementById('find_book_button');
 	findBookButton.onclick = findBookButtonFunction;
+
+	var sellBookButton = document.getElementById('sell_book_button');
+	sellBookButton.onclick = sellBookButtonFunction;
 }
 
 function buyFunction() {
@@ -21,11 +24,70 @@ function sellFunction() {
 	buyInfo.style.display = 'none';
 	var sellInfo = document.getElementById('sell_info');
 	sellInfo.style.display = 'block';
-	var searchResult = document.getElementById('search_result');
+	var searchResult = document.getElementById('search_results');
 	searchResult.style.display = 'none';
 }
 
 function findBookButtonFunction() {
-	var searchResult = document.getElementById('search_result');
-	searchResult.style.display = 'block';
+	var verified = verifySubject('subject_input_buy', 'subject_buy_error') && 
+		verifyCourseNum('course_num_input_buy', 'course_num_buy_error');
+
+	var searchResult = document.getElementById('search_results');	
+	if (verified) {
+		searchResult.style.display = 'block';
+	} else {
+		searchResult.style.display = 'none';
+	}
+}
+
+function sellBookButtonFunction() {
+	var verified = verifySubject('subject_input_sell', 'subject_sell_error') &&
+		verifyCourseNum('course_num_input_sell', 'course_num_sell_error');
+
+	if (verified) {
+		console.log("OK");
+		window.location.href = 'sell_confirm.html';
+	}
+}
+
+function verifySubject(inputId, errorId) {
+	var subject = document.getElementById(inputId).value;
+	var subjectRegex = /^[a-zA-Z]{2,5}$/
+	if (!subjectRegex.test(subject)) {
+		if (!document.getElementById(errorId).hasChildNodes()) {
+			var errorMessage = document.createElement('p');
+			errorMessage.innerHTML = "Invalid subject";
+			errorMessage.id = errorId + "_message";
+			errorMessage.style.textAlign = "left";
+			errorMessage.style.marginLeft = '18%';
+			errorMessage.style.color = 'red';
+			document.getElementById(errorId).appendChild(errorMessage);
+		}
+		return false;
+	} else if (document.getElementById(errorId).hasChildNodes()) {
+		var errorMessage = document.getElementById(errorId + "_message");
+		errorMessage.parentNode.removeChild(errorMessage);
+	}
+	return true;
+}
+
+function verifyCourseNum(inputId, errorId) {
+	var courseNum = document.getElementById(inputId).value;
+	var courseNumRegex = /^\d{3}$/;
+	if (!courseNumRegex.test(courseNum)) {
+		if (!document.getElementById(errorId).hasChildNodes()) {
+			var errorMessage = document.createElement('p');
+			errorMessage.innerHTML = "Course number must be numeric";
+			errorMessage.id = errorId + "_message";
+			errorMessage.style.textAlign = "left";
+			errorMessage.style.marginLeft = '18%';
+			errorMessage.style.color = 'red';
+			document.getElementById(errorId).appendChild(errorMessage);
+		}
+		return false;
+	} else if (document.getElementById(errorId).hasChildNodes()) {
+		var errorMessage = document.getElementById(errorId + "_message");
+		errorMessage.parentNode.removeChild(errorMessage);
+	}
+	return true;
 }
