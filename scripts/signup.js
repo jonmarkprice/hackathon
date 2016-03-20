5ee7ec1e650d1f9879008d167788edb49474ab64
Jeremy Falloon <jkf6576@truman.edu>
2016-03-19 19:30:24 -0500

    incorporated verification for truman email

--- scripts/signup.js
+++ scripts/signup.js
@@ -3,16 +3,25 @@ window.onload = function() {
 	signUpButton.onclick = signUp;
 };
 
-var emailRegex = /^[a-z]{,4}\d{4}$/;
-
 function signUp() {
 	var email = document.getElementById('email_input');
 	var emailValue = email.value;
+	var emailRegex = /^[a-z]{3,4}\d{4}@truman.edu$/;
 
 	if(!emailRegex.test(emailValue)) {
-		var emailError = document.createElement('p');
-		emailError.innerHTML = "Error in the email";
-		email.appendChild(emailError);
+		if(!document.getElementById('email_error').hasChildNodes()) {
+			var emailError = document.createElement('p');
+			emailError.innerHTML = "Error in the email.";
+			emailError.id = "email_error_message";
+			emailError.style.textAlign = 'left';
+			emailError.style.marginLeft = '18%';
+			emailError.style.color = 'red';
+			var emailErrorDiv = document.getElementById('email_error');
+			emailErrorDiv.appendChild(emailError);
+		}
 	}
-
+	else if(document.getElementById('email_error').hasChildNodes()) {
+		var emailError = document.getElementById('email_error_message');
+		emailError.parentNode.removeChild(emailError);
+	}
 }
